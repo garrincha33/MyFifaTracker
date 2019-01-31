4f15372693603a38741ee47c59e23bd30f265e3b
Richard Price <mrspondooli@gmail.com>
2019-01-31 18:22:48 +0000

    Initial Commit

---
+++ MyFifaTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFifaTracker
+//
+//  Created by Richard Price on 31/01/2019.
+//  Copyright © 2019 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
